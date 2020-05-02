1685b577c93b34c1ac71d79564bb03327371c989
Michael <michael@managedkaos.com>
2020-05-02 00:24:46 -0700

    Update function

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
-ZIP  = script.zip
-SRC  = script.py
+FUNCTION = script
+ZIP  = $(FUNCTION).zip
+SRC  = $(FUNCTION).py
 .PHONY: all plan $(AUTO)
 
 $(ZIP): $(SRC)
 	zip -X $(ZIP) $(SRC)
 
 clean:
-	rm $(ZIP)
+	rm -vf $(ZIP) response.json
+
+invoke:
+	aws lambda invoke --function-name $(FUNCTION) --invocation-type Event response.json
 
 plan: $(ZIP)
 	terraform $@
--- script.py
+++ script.py
@@ -3,10 +3,10 @@ import sys
 import json
 
 def lambda_handler(event, context):
-    return {
-            'statusCode': 200,
-            'body': json.dumps(event['pathParameters'])
-            }
+    if 'pathParameters' in event:
+        return { 'statusCode': 200, 'body': json.dumps(event['pathParameters']) }
+    else:
+        return { 'statusCode': 404, 'body': json.dumps('No path paramters found') }
 
 if __name__ == '__main__':
      print(sys.argv)
